727c5951966542cb6f5f74099457b4cbe5ff7162
kevincrys <kevin.c.costa@edu.unirio.br>
2022-02-10 17:38:38 -0300

    correções

--- Extract.js
+++ Extract.js
@@ -8,7 +8,7 @@ var ultimaAtt =0;
 //se falhar tenta mais  2 vezes
   for(let i=0;i<3;i++)
   {
-    //coleta o artigo mais recente
+    //coleta o id do artigo mais recente
   try{
       var dados= await axios.get('https://api.spaceflightnewsapi.net/v3/articles/');
 
@@ -19,7 +19,9 @@ var ultimaAtt =0;
 
   }
 }
-for(let i=7033;i<ultimaAtt;i++){
+//percorre todo os artigos da api e armazena no Banco de dados
+for(let i=11539;i<ultimaAtt;i++){
+  //se falhar tenta mais  2 vezes
 for(let j=0;j<2;j++){
 try{
     var dados= await axios.get('https://api.spaceflightnewsapi.net/v3/articles/'+i);
